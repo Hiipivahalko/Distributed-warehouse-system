c576847dc70679da5f4deff1d0f94a536e1bea9f
Ossi Siipola <siipolaossi@gmail.com>
2021-11-20 14:42:02 +0200

    front show warehouses

--- warehouse-front/src/App.js
+++ warehouse-front/src/App.js
@@ -8,43 +8,37 @@ const App = () => {
 
   const [ products, setProducts ] = useState(null);
 
-  useEffect( () => {
-    const find_products = async () => {
-      try {
-        console.log(`here`);
-        const result = await axios.get(`${process.env.REACT_APP_ORDER_SERVICE_URL}/warehouses/`);
-        const data = result.data[0];
-        console.log('headers:', result.headers);
-        console.log(`result:`, data);
-        console.log(`result: ${JSON.stringify(data)}`);
-        //setProducts(result.data);
-      } catch (err) {
-        console.log(err.message);
-      }
+  const find_products = async () => {
+    try {
+      console.log(`here`);
+      const result = await axios.get(`${process.env.REACT_APP_ORDER_SERVICE_URL}/warehouses/`);
+      setProducts(result.data);
+    } catch (err) {
+      console.log(err.message);
     }
+  }
+
+  useEffect( () => { 
     find_products();
   }, [])
 
-  let test = {
-    'a': 0,
-    'b': 1
+  const fectProducts = async (event) => {
+    event.preventDefault()
+    console.log('button cliked');
+    await find_products();
+    console.log('data fetched');
   }
-
-  let a ='aa'
-
-  console.log(`test: ${test}`);
-  console.log(a)
-  console.log(test)
 
   return (
     <div>
       <h1>DisSys Warehouse</h1>
+      <button onClick={fectProducts}>Fetch warehouses</button>
       {products ? 
         <>
-          <h3>Products:</h3>
+          <h3>Warehouses:</h3>
           <ul>
             {products.map(p => 
-              <li key={p}>{p}</li>  
+              <li key={p.id}>{p.location}</li>  
             )}
           </ul>
         </>
